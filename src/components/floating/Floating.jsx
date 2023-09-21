7dfc91851eec79abf23e6543fd573b89b7b1fadb
UKIZURU Mustapha <mr.ukizuru@gmail.com>
2023-09-21 15:51:43 -0600

    floating

--- src/components/floating/Floating.jsx
+++ src/components/floating/Floating.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import './floating.css'
-const Floating = ({image, text}) => {
+// import React from 'react'
+// import './floating.css'
+// const Floating = ({image, text}) => {
+//   return (
+//     <div className='floatingdiv'>
+//      <img src={image} alt="" />
+//      <span>
+//        <small> {text}</small>
+//      </span>
+//         </div>
+//   )
+// }
+// export default Floating
+
+
+import React, { useState } from 'react';
+import './floating.css'; // Import your CSS file for styling
+import Content from '../../assets/images/content-creator.png'
+import Coding from '../../assets/images/app-development.png'
+import Developer from '../../assets/images/app-development.png'
+function Floating() {
+  const [isFloating, setIsFloating] = useState(false);
+
+  const toggleFloating = () => {
+    setIsFloating(!isFloating);
+  };
+
   return (
-    <div className='floatingdiv'>
-     <img src={image} alt="" />
-     <span>
-       <small> {text}</small>
-     </span>
-        </div>
-  )
+    <div className={`floating-container ${isFloating ? 'floating' : ''}`}>
+      <div className="floating-div web-development">
+        <img src={Coding} alt="Web Development" />
+        <small>Web Development</small>
+      </div>
+      <div className="floating-div content-creation">
+        <img src={Developer} alt="Content Creation" />
+        <small>Content Creation</small>
+      </div>
+      <div className="floating-div ux-design">
+        <img src={Developer} alt="UX Design" />
+        <small>UX  Design</small>
+      </div>
+      <div className="floating-div backend-development">
+        <img src={Content} alt="Backend Development" />
+        <small>Backend Development</small>
+      </div>
+    </div>
+  );
 }
-export default Floating
+
+export default Floating;

2fed30e26cbd9f786d7bc4cc57336997d0b81903
UKIZURU Mustapha <mr.ukizuru@gmail.com>
2023-09-21 17:30:39 -0600

    All Changes Navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import './nav.css'
 import  {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
+import {BsInfoLg} from 'react-icons/bs'
 import {BiBookBookmark} from 'react-icons/bi'
 import {RiServiceFill} from 'react-icons/ri'
 import {TiMessages} from 'react-icons/ti'
+import {BiUserPlus} from 'react-icons/bi'
 import { useState } from 'react';
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
 
-<nav>
+<nav className='nav__navbar'>
 
   <a href="#"onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
-  <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+  <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BsInfoLg/></a>
   <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBookBookmark/></a>
   <a href="#service" onClick={() => setActiveNav('#service')} className={activeNav === '#service' ? 'active' : ''}><RiServiceFill/></a>
   <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TiMessages/></a>
+  <a href="#join__Me" onClick={() => setActiveNav('#join__Me')} className={activeNav === '#join__Me' ? 'active' : ''}><BiUserPlus/></a>
  </nav>
   )
 }
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -7,26 +7,38 @@ import './navbar.css'
 //  import {BiBookBookmark} from 'react-icons/bi'
 //  import {RiServiceFill} from 'react-icons/ri'
 //  import {TiMessages} from 'react-icons/ti'
+import {BiUserPlus} from 'react-icons/bi'
+import { useState } from 'react';
+import meavatar from '../../assets/images/android-chrome-512x512.png'
 // import { useState } from 'react';
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
 <div className="n-wrapper container navbar__container">
     <div className="n-left">
-        <div className="n-name">Mr. Ukizuru</div>
+        <div className="n-name"> 
+        <img className='me__avatar' src={meavatar} alt=" testimonial image" /><a href="#"onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>Mr. Ukizuru</a></div>
     </div>
     <div className='n-right'>
         <div className="n-list">
             <ul>
-                <li>Home</li>
-                <li>Services</li>
-                <li>Experience</li>
-                <li>Portfolio</li>
-                <li>About</li>
-                <li>contact</li>
+                <li><a href="#"onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>Home</a></li>
+                <li><a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>About</a></li>
+                <li><a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>Service</a></li>
+                <li> <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>Experience</a></li>
+                <li><a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>Portfolio</a></li>
+                <li><a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>Contact</a></li>
             </ul>
-     
         </div>
-        <button type="button" className='btn btn-primary'>send message</button>
+
+
+        <button class="joinbtn">
+  <span class="join__me">
+  <BiUserPlus className='join__me-icon'/>
+    {/* <svg viewBox="0 0 384 512" height="0.9em" class="icon"><path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"></path></svg> */}
+  </span>
+  <p class="text">Join Me</p>
+</button>
     </div>
 </div>
   )

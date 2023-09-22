f6334194a3e0f0d784b286bb8ce979574d4998a6
UKIZURU Mustapha <mr.ukizuru@gmail.com>
2023-09-22 09:42:12 -0600

    Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import './App.css'
-import Floating from './components/floating/Floating'
 import Navbar from './components/nav/Navbar'
 import Header from './components/header/Header'
 import About from './components/about/About'
@@ -10,41 +8,49 @@ import Footer from './components/footer/Footer'
 import Nav from './components/nav/Nav'
 import Service from './components/service/Service'
 import Testimonials from './components/testimonial/Testimonials'
-import Portfolio  from './components/portfolio/Portfolio'
+// import Portfolio  from './components/portfolio/Portfolio'
+import Projects from "./components/projects/index";
+import ProjectDetails from "./components/projectDetails/index";
+import styled from "styled-components";
+
+import React, { useState } from 'react';
+// import { useMediaQuery } from 'react-responsive';
+
+// export const useBreakpoints = () => {
+//   const isMobileSmall = useMediaQuery({ query: '(max-width: 325px)' });
+//   const isMobileMid = useMediaQuery({ query: '(max-width: 375px)' });
+//   const isMobileFloor = useMediaQuery({ query: '(max-width: 425px)' });
+
+//   const isTabletFloor = useMediaQuery({ query: '(max-width: 426px)' });
+//   const isTabletMid = useMediaQuery({ query: '(max-width: 768px)' });
+//   const isTabletCeil = useMediaQuery({ query: '(max-width: 1024px)' });
+
+//   const isLaptopFloor = useMediaQuery({ query: '(max-width: 1025px)' });
+//   const isLaptopCeil = useMediaQuery({ query: '(max-width: 1440px)' });
+
+//   const isXHDFloor = useMediaQuery({ query: '(max-width: 1441px)' });
+//   const isXHDCeil = useMediaQuery({ query: '(max-width: 4096px)' });
 
 
-import { useMediaQuery } from 'react-responsive';
 
-export const useBreakpoints = () => {
-  const isMobileSmall = useMediaQuery({ query: '(max-width: 325px)' });
-  const isMobileMid = useMediaQuery({ query: '(max-width: 375px)' });
-  const isMobileFloor = useMediaQuery({ query: '(max-width: 425px)' });
 
-  const isTabletFloor = useMediaQuery({ query: '(max-width: 426px)' });
-  const isTabletMid = useMediaQuery({ query: '(max-width: 768px)' });
-  const isTabletCeil = useMediaQuery({ query: '(max-width: 1024px)' });
-
-  const isLaptopFloor = useMediaQuery({ query: '(max-width: 1025px)' });
-  const isLaptopCeil = useMediaQuery({ query: '(max-width: 1440px)' });
-
-  const isXHDFloor = useMediaQuery({ query: '(max-width: 1441px)' });
-  const isXHDCeil = useMediaQuery({ query: '(max-width: 4096px)' });
-
-  return {
-    isMobileSmall,
-    isMobileMid,
-    isMobileFloor,
-    isTabletFloor,
-    isTabletMid,
-    isTabletCeil,
-    isLaptopFloor,
-    isLaptopCeil,
-    isXHDFloor,
-    isXHDCeil,
-  };
-};
+//   return {
+//     isMobileSmall,
+//     isMobileMid,
+//     isMobileFloor,
+//     isTabletFloor,
+//     isTabletMid,
+//     isTabletCeil,
+//     isLaptopFloor,
+//     isLaptopCeil,
+//     isXHDFloor,
+//     isXHDCeil,
+//   };
+// };
 
 const App = () =>{
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  console.log(openModal)
   return (
     <>
 <Navbar/>
@@ -53,8 +59,12 @@ const App = () =>{
 <About/>
 <Experience/>
 <Service/>
-<Portfolio/>
-<Testimonials/>
+{/* <Portfolio/> */}
+{/* <Testimonials/> */}
+<Projects openModal={openModal} setOpenModal={setOpenModal} />
+{openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
 <Contact/>
 <Footer/>
     </>
--- src/components/testimonial/Testimonials.jsx
+++ src/components/testimonial/Testimonials.jsx
@@ -1,66 +1,66 @@
-import React from 'react'
-import './testimonial.css'
-// import Swiper core and required modules
-import {Pagination} from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import AVTR from '../../assets/images/android-chrome-512x512.png'
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-const data = [
-{
-avatar: AVTR,
-name: 'Tina Snow',
-review: 'Working with Designer s Name was a game-changer. Their UI/UX expertise elevated our project, creating an intuitive and visually stunning experience. '
-},
-{
-  avatar: AVTR,
-  name: 'Tina Snow',
-  review: 'Working with Designer s Name was a game-changer. Their UI/UX expertise elevated our project, creating an intuitive and visually stunning experience. '
-},
-  {
-    avatar: AVTR,
-    name: 'Tina Snow',
-    review: 'Working with Designer s Name was a game-changer. Their UI/UX expertise elevated our project, creating an intuitive and visually stunning experience. '
-  },
-    {
-      avatar: AVTR,
-      name: 'Tina Snow',
-      review: 'Working with Designer s Name was a game-changer. Their UI/UX expertise elevated our project, creating an intuitive and visually stunning experience. '
-    }
-]
+// import React from 'react'
+// import './testimonial.css'
+// // import Swiper core and required modules
+// import {Pagination} from 'swiper/modules';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import AVTR from '../../assets/images/android-chrome-512x512.png'
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// const data = [
+// {
+// avatar: AVTR,
+// name: 'Tina Snow',
+// review: 'Working with Designer s Name was a game-changer. Their UI/UX expertise elevated our project, creating an intuitive and visually stunning experience. '
+// },
+// {
+//   avatar: AVTR,
+//   name: 'Tina Snow',
+//   review: 'Working with Designer s Name was a game-changer. Their UI/UX expertise elevated our project, creating an intuitive and visually stunning experience. '
+// },
+//   {
+//     avatar: AVTR,
+//     name: 'Tina Snow',
+//     review: 'Working with Designer s Name was a game-changer. Their UI/UX expertise elevated our project, creating an intuitive and visually stunning experience. '
+//   },
+//     {
+//       avatar: AVTR,
+//       name: 'Tina Snow',
+//       review: 'Working with Designer s Name was a game-changer. Their UI/UX expertise elevated our project, creating an intuitive and visually stunning experience. '
+//     }
+// ]
 
-const Testimonials = () => {
-  return (
-    <section id='testimonials'>
-<h5>Review from Clients</h5>
-<h2>Testimonials</h2>
-<div className="container testimonials__container">
-<Swiper
-   // install Swiper modules
-   modules={[Pagination]}
-   spaceBetween={40}
-   slidesPerView={1}
-   pagination={{ clickable: true }}>
+// const Testimonials = () => {
+//   return (
+//     <section id='testimonials'>
+// <h5>Review from Clients</h5>
+// <h2>Testimonials</h2>
+// <div className="container testimonials__container">
+// <Swiper
+//    // install Swiper modules
+//    modules={[Pagination]}
+//    spaceBetween={40}
+//    slidesPerView={1}
+//    pagination={{ clickable: true }}>
 
-{
-  data.map(({ avatar, name, review}, index) => {
+// {
+//   data.map(({ avatar, name, review}, index) => {
 
-return (
-<SwiperSlide key={index} className='testimonial'>
-  <div className='client__avatar'>
-    <img src={avatar} alt=" testimonial image" />
-  </div>
-  <h5 className='client__name'>{name}</h5>
-<small className='client__review'>{review}</small>
+// return (
+// <SwiperSlide key={index} className='testimonial'>
+//   <div className='client__avatar'>
+//     <img src={avatar} alt=" testimonial image" />
+//   </div>
+//   <h5 className='client__name'>{name}</h5>
+// <small className='client__review'>{review}</small>
 
-</SwiperSlide>
-)
-})
-}
-</Swiper>
-</div>
-    </section>
-  )
-}
-export default Testimonials
+// </SwiperSlide>
+// )
+// })
+// }
+// </Swiper>
+// </div>
+//     </section>
+//   )
+// }
+// export default Testimonials

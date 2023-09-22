c4542529a0f473d3dab19cbaa95d5373a94768b2
UKIZURU Mustapha <mr.ukizuru@gmail.com>
2023-09-22 17:20:45 -0600

    Commit

--- src/components/testimonial/Testimonials.jsx
+++ src/components/testimonial/Testimonials.jsx
@@ -1,32 +1,9 @@
-import React from 'react';
-import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css';
-import './testimonials.css'; // Import your custom styles for testimonials if needed
+import React from 'react'
 
-// Initialize Swiper core components
-SwiperCore.use([Navigation, Pagination, Autoplay]);
-
-const Testimonials = ({ testimonials }) => {
+function Testimonials() {
   return (
-    <div className="testimonials-container">
-      <Swiper
-        navigation
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 5000 }}
-        loop={true}
-      >
-        {testimonials.map((testimonial, index) => (
-          <SwiperSlide key={index}>
-            <div className="testimonial">
-              <p className="testimonial-text">{testimonial.text}</p>
-              <p className="testimonial-author">{testimonial.author}</p>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  );
-};
+    <div>Testimonials</div>
+  )
+}
 
-export default Testimonials;
+export default Testimonials
